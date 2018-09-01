8b417c8f4bdb308bd6d777eeb54ef44508b8f9f6
Glitch (elm-sample-app) <none>
2018-09-01 18:10:03 +0000

    👙🎋 Checkpoint ./README.md:379055/573 ./package.json:379055/2

--- source/main.elm
+++ source/main.elm
@@ -1,30 +1,39 @@
-import Html exposing (Html, button, div, text, a, img)
-import Html.Attributes exposing (href, alt, src)
+module Main exposing (Msg(..), main, update, view)
+
+import Html exposing (Html, a, button, div, img, text)
+import Html.Attributes exposing (alt, href, src)
 import Html.Events exposing (onClick)
 
-main =
-  Html.beginnerProgram { model = 0, view = view, update = update }
 
-type Msg = Increment | Decrement
+main =
+    Html.beginnerProgram { model = 0, view = view, update = update }
+
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 view model =
-  div []
-    [ div [] [ text "Counter" ]
-    , button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    , div [] [ a 
-                  [ href "https://glitch.com/edit/#!/remix/elm-sample-app"
-                  , alt "Remix On Glitch"
-                  ]
-                [ img [ src "https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg" ] [] ] 
-              ]
-    ]
+    div []
+        [ div [] [ text "Counter" ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , div []
+            [ a
+                [ href "https://glitch.com/edit/#!/remix/elm-sample-app"
+                , alt "Remix On Glitch"
+                ]
+                [ img [ src "https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg" ] [] ]
+            ]
+        ]
