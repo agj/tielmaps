ced5fed728e7109532de5bb73d504ebd8bd4e335
agj <ale@agj.cl>
2021-05-01 22:44:22 -0400

    Add `encode` to Canvas.

--- source/elm/Canvas.elm
+++ source/elm/Canvas.elm
@@ -1,6 +1,7 @@
 module Canvas exposing (..)
 
 import Array exposing (Array)
+import Json.Encode as Encode exposing (Value)
 
 
 type Canvas
@@ -32,6 +33,15 @@ pixel x y (Canvas w h pixels) =
         |> Array.get (pos h x y)
 
 
+encode : Canvas -> Value
+encode (Canvas w h pixels) =
+    Encode.object
+        [ ( "width", w |> Encode.int )
+        , ( "height", h |> Encode.int )
+        , ( "pixels", pixels |> Encode.array boolEncoder )
+        ]
+
+
 
 -- SETTERS
 
@@ -50,3 +60,12 @@ paintPixel x y on (Canvas w h pixels) =
 pos : Int -> Int -> Int -> Int
 pos h x y =
     (h * y) + x
+
+
+boolEncoder : Bool -> Value
+boolEncoder value =
+    if value then
+        Encode.int 1
+
+    else
+        Encode.int 0
