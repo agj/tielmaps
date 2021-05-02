49c160f127bb75bf55efc160827d8f164ae0c2bd
agj <ale@agj.cl>
2021-05-01 23:07:22 -0400

    Add paint canvas JS code.

--- source/js/index.js
+++ source/js/index.js
@@ -5,7 +5,28 @@ const app = Elm.Main.init({
   flags: {
     viewport: {
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
+    },
+  },
+});
+
+app.ports.paintCanvas.subscribe(({ width, height, pixels }) => {
+  const imageData = ctx.createImageData(width, height);
+  for (let x = 0; x < width; ++x) {
+    for (let y = 0; y < height; ++y) {
+      const cur = getPixel(height, x, y, pixels);
+      imageData.data[x * 4 + 0] = cur * 255; // red
+      imageData.data[x * 4 + 1] = cur * 255; // green
+      imageData.data[x * 4 + 2] = cur * 255; // blue
+      imageData.data[x * 4 + 3] = 255; // alpha
     }
   }
+
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.putImageData(imageData, 0, 0);
 });
+
+const getPixel = (height, x, y, pixels) => {
+  return pixels[height * y + x];
+};
